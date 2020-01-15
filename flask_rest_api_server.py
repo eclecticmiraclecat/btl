de78ec5dec0f4c101b66e988831d712b882ab333
fcs.my.benedict.baskar <benedict.baskar@planet1world.com>
2020-01-15 20:57:54 +0800

    add index route

--- flask_rest_api_server.py
+++ flask_rest_api_server.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+    return 'Hello'
 
 if __name__ == '__main__':
     app.run()
--- rest_api_server.py
+++ rest_api_server.py
@@ -1,5 +1,9 @@
 from bottle import *
 
+@route('/')
+def welcome():
+    return 'Hello'
+
 if __name__ == '__main__':
 
     run(host='localhost', port=8080)
